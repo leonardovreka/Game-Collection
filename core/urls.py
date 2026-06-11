3df1298c6cd4d5f21502ebe0b51275f2237ca1b3
Leonardo Vreka <leonardovreka@gmail.com>
2026-06-11 22:32:43 +0300

    Adding the standard games list + realizing i want to make it a multi user collection

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('ps3/', views.ps3_collection, name='ps3_collection'),
+    path('ps3/standard/', views.standard_games, name='standard_games'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request, 'landing.html')
+
+
+def ps3_collection(request):
+    return render(request, 'ps3_collection.html')
+
+
+def standard_games(request):
+    return render(request, 'standard_games.html')
